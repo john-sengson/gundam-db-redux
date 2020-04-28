4dd69f1f07c3ab147ab0e31dd47e43e33443fccd
john-sengson <j10i_freedom@hotmail.com>
2020-04-29 00:29:25 +1200

    add GundamList component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import gundam from '../../data/gundam.js'
 
 import Header from './Header'
+import GundamList from './GundamList.jsx'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <GundamList gundam={gundam} />
     </div>
   )
 }
---
+++ client/components/GundamList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+// const gundam = { gundam }
+
+class GundamList extends React.Component {
+
+  render() {
+    const { gundam } = this.props.gundam
+    return (
+      <div>
+        {gundam.map((gundam) => {
+          return (
+            <>
+              <p key={gundam.id}>{gundam.unit}</p>
+              <p key={gundam.id}>Grade:{gundam.grade}</p>
+              <p key={gundam.id}>Scale:{gundam.scale}</p>
+              <p key={gundam.id}>Price:{gundam.price}</p>
+              <img key={gundam.id} src={gundam.picture} alt="" />
+              <br />
+              <a href="/cart" classname="cart-link">Add</a>
+              <br />
+              <br />
+            </>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+export default GundamList;

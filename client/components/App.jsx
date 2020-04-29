85a8b45b73cbb1109926bf5b3cb85cda034348d3
john-sengson <j10i_freedom@hotmail.com>
2020-04-30 08:31:46 +1200

    installed react router dom and add routes to index.js

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import gundam from '../../data/gundam.js'
 
 import Header from './Header'
 import GundamList from './GundamList.jsx'
+import Cart from './Cart'
 
 const App = () => {
   return (
     <div>
       <Header />
       <GundamList gundam={gundam} />
+      <Route exact path='/cart' component={Cart} />
     </div>
   )
 }
---
+++ client/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Cart extends React.Component {
+
+  render() {
+    const { gundam } = this.props.gundam
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default Cart
